58940d054d79b5b3ebbc2f7acc7b87e194a1f17e
jimmy.huangjin <jimmy.huangjin@gmail.com>
2014-08-27 16:51:26 +0800

    init commit

---
+++ XiaoHuaApp/XHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XHAppDelegate.h
+//  XiaoHuaApp
+//
+//  Created by HuangJin on 14-8-27.
+//  Copyright (c) 2014年 Ebooom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XiaoHuaApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XiaoHuaApp
+//
+//  Created by HuangJin on 14-8-27.
+//  Copyright (c) 2014年 Ebooom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XHAppDelegate class]));
+    }
+}
